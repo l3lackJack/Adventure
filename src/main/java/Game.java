bd79f61b3291eaf9c015c75688dba636c804a35a
l3lackJack <gabrielionuturs@iesebre.com>
2021-10-22 18:44:08 +0200

    Subiendo juego

---
+++ src/main/java/Game.java
@@ -0,0 +1,322 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game {
+    JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
+    JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN,90);
+    Font normalFont = new Font("Times new Roman", Font.PLAIN,28);
+    Font littleFont = new Font("Times new Roman", Font.BOLD, 15);
+    Font buttonsFont = new Font("Times new roman", Font.PLAIN, 17);
+    JButton startButton, choice1, choice2, choice3, choice4;
+    JTextArea mainTextArea;
+    int playerHP, monsterHP;
+    String playerWeapon, position;
+
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
+    ChoiceHandler choiceHandler = new ChoiceHandler();
+
+    public static void main(String[] args) {
+        new Game();
+    }
+    public Game(){
+        window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+        window.setVisible(true);
+        con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100,100,600,150);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("Aventura");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300,400,200,100);
+        startButtonPanel.setBackground(Color.black);
+
+
+        startButton = new JButton("Inicio");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(normalFont);
+        startButton.addActionListener(tsHandler);
+        startButton.setFocusPainted(false);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+
+
+        con.add(titleNamePanel);
+        con.add(startButtonPanel);
+    }
+    public void createGameScreen(){
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100,100,600,250);
+        mainTextPanel.setBackground(Color.black);
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea();
+        mainTextArea.setBounds(100,100,600,250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(275,350,250,115);
+        choiceButtonPanel.setBackground(Color.black);
+        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        con.add(choiceButtonPanel);
+
+        choice1 = new JButton("Opcion 1");
+        choice1.setBackground(Color.black);
+        choice1.setForeground(Color.white);
+        choice1.setFont(buttonsFont);
+        choice1.setFocusPainted(false);
+        choice1.addActionListener(choiceHandler);
+        choice1.setActionCommand("c1");
+        choiceButtonPanel.add(choice1);
+
+        choice2 = new JButton("Opcion 2");
+        choice2.setBackground(Color.black);
+        choice2.setForeground(Color.white);
+        choice2.setFont(buttonsFont);
+        choice2.setFocusPainted(false);
+        choice2.addActionListener(choiceHandler);
+        choice2.setActionCommand("c2");
+        choiceButtonPanel.add(choice2);
+
+
+        choice3 = new JButton("Opcion 3");
+        choice3.setBackground(Color.black);
+        choice3.setForeground(Color.white);
+        choice3.setFont(buttonsFont);
+        choice3.setFocusPainted(false);
+        choice3.addActionListener(choiceHandler);
+        choice3.setActionCommand("c3");
+        choiceButtonPanel.add(choice3);
+
+        choice4 = new JButton("Opcion 4");
+        choice4.setBackground(Color.black);
+        choice4.setForeground(Color.white);
+        choice4.setFont(buttonsFont);
+        choice4.setFocusPainted(false);
+        choice4.addActionListener(choiceHandler);
+        choice4.setActionCommand("c4");
+        choiceButtonPanel.add(choice4);
+
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100,15,600,50);
+        playerPanel.setBackground(Color.black);
+        playerPanel.setLayout(new GridLayout(1,4));
+        con.add(playerPanel);
+
+        hpLabel = new JLabel("Vida:");
+        hpLabel.setFont(littleFont);
+        hpLabel.setForeground(Color.white);
+        playerPanel.add(hpLabel);
+
+        hpLabelNumber = new JLabel();
+        hpLabelNumber.setFont(littleFont);
+        hpLabelNumber.setForeground(Color.white);
+        playerPanel.add(hpLabelNumber);
+
+        weaponLabel = new JLabel("Arma:");
+        weaponLabel.setFont(littleFont);
+        weaponLabel.setForeground(Color.white);
+        playerPanel.add(weaponLabel);
+
+        weaponLabelName = new JLabel();
+        weaponLabelName.setFont(littleFont);
+        weaponLabelName.setForeground(Color.white);
+        playerPanel.add(weaponLabelName);
+
+        playerSetup();
+    }
+
+    public void playerSetup(){
+        playerHP = 15;
+        monsterHP =20;
+        playerWeapon = "Sin arma";
+        weaponLabelName.setText(playerWeapon);
+        hpLabelNumber.setText(""+ playerHP);
+
+        townGate();
+    }
+
+    public void townGate(){
+        position = "townGate";
+        mainTextArea.setText("Estas en la puerta de la ciudad, \nun guardia se encuentra delante tuyo \n\n¿Que haces?");
+        choice1.setText("Habla con el guardia");
+        choice2.setText("Pegarle al guardia");
+        choice3.setText("Irte");
+        choice4.setText("");
+    }
+
+    public void talkGuard(){
+        position = "talkGuard";
+        mainTextArea.setText("Guardia: Hola señor, nunca lo he visto\npor aqui, lo siento pero no puedo dejar\na un extraño entrar el el pueblo");
+        choice1.setText("Volver");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void attackGuard(){
+        position = "attackGuard";
+        mainTextArea.setText("Guardia: Vas a ver lo que es bueno\n\nEl guardia te ha pegado una paliza \n (Has recibido 3 de daño)");
+        playerHP = playerHP-3;
+        hpLabelNumber.setText(""+playerHP);
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void crossRoad(){
+        position="crossRoad";
+        mainTextArea.setText("Estas en un cruce de caminos.\nSi vas al sur volveras a la ciudad");
+        choice1.setText("Ir hacia el norte");
+        choice2.setText("Ir hacia el este");
+        choice3.setText("Ir hacia el sur");
+        choice4.setText("Ir hacie el oeste");
+    }
+
+    public void north(){
+        position = "north";
+        mainTextArea.setText("Hay un rio delante tuyo\nBebes agua y descansas al lado del rio\n\n (Recuperas 2 de vida)");
+        playerHP = playerHP+2;
+        hpLabelNumber.setText(""+playerHP);
+        choice1.setText("Volver al cruce");
+        choice2.setText("Cruzar el rio");
+        choice3.setText("Adentrarse en el bosque");
+        choice4.setText("Dormir");
+    }
+
+
+    public void east(){
+        position = "east";
+        mainTextArea.setText("Has caminado hacia el bosque y has encontrado una Espada Larga\n\n (Has obtenido una Espada Larga)");
+        playerWeapon = "Espada Larga";
+        weaponLabelName.setText(playerWeapon);
+        choice1.setText("Ir al oeste");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+    public void west(){
+        position = "west";
+        mainTextArea.setText("Has encontrado un goblin");
+        choice1.setText("Pelear");
+        choice2.setText("Correr");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void fight(){
+        position = "fight";
+        mainTextArea.setText("HP Goblin: " + monsterHP + "\n\nQue vas a hacer?");
+        choice1.setText("Atacar");
+        choice2.setText("Correr");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void playerAttack(){
+        position = "playerAttack";
+
+        int playerDamage = 0;
+
+        playerDamage = new java.util.Random().nextInt(5);
+    }
+
+
+
+    public class TitleScreenHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            createGameScreen();
+        }
+    }
+
+    public class ChoiceHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            String yourChoice = event.getActionCommand();
+            switch (position){
+                case "townGate":
+                    switch (yourChoice){
+                        case "c1": talkGuard(); break;
+                        case "c2": attackGuard();break;
+                        case "c3": crossRoad();break;
+                    }
+                    break;
+                case "talkGuard":
+                    switch (yourChoice){
+                        case "c1": townGate(); break;
+                    }
+                    break;
+                case "attackGuard":
+                    switch (yourChoice){
+                        case "c1" : townGate(); break;
+                    }
+                    break;
+                case "crossRoad":
+                    switch (yourChoice){
+                        case "c1": north();break;
+                        case "c2": east();break;
+                        case "c3": townGate();break;
+                        case "c4": west();break;
+                    }
+                    break;
+                case "north" :
+                    switch (yourChoice){
+                        case "c1" : crossRoad();break;
+                        case "c2" : ;break;
+                        case "c3" : break;
+                        case "c4" : break;
+                    }
+                    break;
+                case "east":
+                    switch (yourChoice){
+                        case "c1" :crossRoad();break;
+                        case "c2" :break;
+                        case "c3" :break;
+                        case "c4" :break;
+                    }
+                    break;
+                case "west":
+                    switch (yourChoice){
+                        case "c1": fight();break;
+                        case "c2": crossRoad();break;
+                    }
+                break;
+                    case "fight":
+                        switch (yourChoice){
+                            case "c1" : break;
+                            case "c2" : crossRoad();break;
+                        }
+
+
+         }
+        }
+    }
+
+
+
+
+}
+
